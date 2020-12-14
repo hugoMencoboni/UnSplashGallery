c141ae38f14ec26745f42b81a611250ba8712046
hugo.M <hugo.mencoboni@gmail.com>
2020-12-13 22:08:04 -0500

    Header title

--- src/Components/TextImage/TextImage.js
+++ src/Components/TextImage/TextImage.js
@@ -2,11 +2,26 @@ import React from 'react';
 import  './TextImage.scoped.scss';
 
 const TextImage = props => {
-    return (
-        <div className="text">
-        {props.text }
-    </div>
-    );
+    let content;
+    switch (props.type) {
+        case 'h1':
+            content = (<h1 className="text">{props.text}</h1>);
+            break;
+        case 'h2':
+            content = (<h2 className="text">{props.text}</h2>);
+            break;
+        case 'h3':
+            content = (<h3 className="text">{props.text}</h3>);
+            break;
+        case 'h4':
+            content = (<h4 className="text">{props.text}</h4>);
+            break;
+        default:
+            content = (<h1 className="text">{props.text}</h1>);
+            break;            
+    }
+
+    return (<div>{content}</div>);
 }
 
 export default TextImage;
